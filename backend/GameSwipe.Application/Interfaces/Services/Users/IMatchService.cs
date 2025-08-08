71062463f32cfdcbf8dac71927e697f6154bd2be
Kkkiittt <khamidov357@gmail.com>
2025-08-08 18:37:24 +0500

    match interface

---
+++ backend/GameSwipe.Application/Interfaces/Services/Users/IMatchService.cs
@@ -0,0 +1,22 @@
+﻿using GameSwipe.Application.Dtos.Match;
+
+namespace GameSwipe.Application.Interfaces.Services.Users;
+
+public interface IMatchService
+{
+	public Task<bool> CreateMatchAsync(MatchWriteDto dto);
+
+	public Task<bool> UpdateMatchAsync(MatchWriteDto dto);
+
+	public Task<bool> DeleteMatchAsync(MatchWriteDto dto);
+
+	public Task<List<MatchGetShortDto>> GetMatchesAllAsync(int page = 1, int pageSize = 50);
+
+	public Task<List<MatchGetShortDto>> GetMatchesAcceptedAsync(int page = 1, int pageSize = 50);
+
+	public Task<List<MatchGetShortDto>> GetMatchesDeclinedAsync(int page = 1, int pageSize = 50);
+
+	public Task<List<MatchGetShortDto>> GetMatchesReceivedAsync(int page = 1, int pageSize = 50);
+
+	public Task<List<MatchGetShortDto>> GetMatchesMutualAsync(int page = 1, int pageSize = 50);
+}

30d4a91650e2f50f8eb449385c260d3755133cc5
Kkkiittt <khamidov357@gmail.com>
2025-08-09 14:12:35 +0500

    worker services

---
+++ backend/GameSwipe.Application/Interfaces/Services/IEmailService.cs
@@ -0,0 +1,6 @@
+﻿namespace GameSwipe.Application.Interfaces.Services;
+
+public interface IEmailService
+{
+	public Task<bool> SendEmailAsync(string email, string subject, string message);
+}
---
+++ backend/GameSwipe.Application/Interfaces/Services/ITokenService.cs
@@ -0,0 +1,10 @@
+﻿using GameSwipe.DataAccess.Entities.Users;
+
+namespace GameSwipe.Application.Interfaces.Services;
+
+public interface ITokenService
+{
+	public string GenerateToken(User user);
+
+	public string RefreshToken(string token);
+}
